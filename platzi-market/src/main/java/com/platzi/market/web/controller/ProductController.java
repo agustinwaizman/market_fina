f19e12ed48541f17d5c98377e3035462b74f781a
Agustin <aguswaizman98@gmail.com>
2022-10-13 12:09:50 -0300

    Last Commit. Adding security by JWT

--- platzi-market/src/main/java/com/platzi/market/web/controller/ProductController.java
+++ platzi-market/src/main/java/com/platzi/market/web/controller/ProductController.java
@@ -11,9 +11,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.net.http.HttpTimeoutException;
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/products")
