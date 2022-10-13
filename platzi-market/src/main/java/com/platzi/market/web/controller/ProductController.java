bb3e4d3d38d9ff7c28e430b1ffce9d4ff7271a5c
Agustin <aguswaizman98@gmail.com>
2022-10-13 12:27:26 -0300

    Last Commit. Adding security by JWT

--- platzi-market/src/main/java/com/platzi/market/web/controller/ProductController.java
+++ platzi-market/src/main/java/com/platzi/market/web/controller/ProductController.java
@@ -2,10 +2,7 @@ package com.platzi.market.web.controller;
 
 import com.platzi.market.domain.Product;
 import com.platzi.market.domain.service.ProductService;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
+import io.swagger.annotations.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,14 +17,14 @@ public class ProductController {
     private ProductService productService;
 
     @GetMapping()
-    @ApiOperation("Get all supermarket products || Muestra todos los productos de supermercado")
+    @ApiOperation(value = "Get all supermarket products || Muestra todos los productos de supermercado", authorizations = { @Authorization(value="JWT") })
     @ApiResponse(code = 200, message = "OK")
     public ResponseEntity< List<Product>> getAll() {
         return new ResponseEntity<>(productService.getAll(), HttpStatus.OK);
     }
 
     @GetMapping("/{productId}")
-    @ApiOperation("Search a product with an ID || buscar un producto por su ID")
+    @ApiOperation(value = "Search a product with an ID || buscar un producto por su ID", authorizations = { @Authorization(value="JWT") })
     @ApiResponses({
             @ApiResponse(code = 200, message = "OK"),
             @ApiResponse(code = 404, message = "PRODUCT NOT FOUND"),
@@ -37,19 +34,19 @@ public class ProductController {
     }
 
     @GetMapping("/category/{categoryId}")
-    @ApiOperation("Search all products corresponding to a category || Buscar todos los productos correspondientes a una categoria")
+    @ApiOperation(value = "Search all products corresponding to a category || Buscar todos los productos correspondientes a una categoria", authorizations = { @Authorization(value="JWT") })
     public ResponseEntity<List<Product>> getByCategory(@ApiParam(value = "The id of the product", required = true, example = "7") @PathVariable("categoryId") int categoryId){
         return productService.getByCategory(categoryId).map(products -> new ResponseEntity<>(products, HttpStatus.OK)).orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
     @PostMapping("/save")
-    @ApiOperation("Create a new supermarket product || Crear un nuevo producto del supermercado")
+    @ApiOperation(value = "Create a new supermarket product || Crear un nuevo producto del supermercado", authorizations = { @Authorization(value="JWT") })
     public ResponseEntity<Product> save(@RequestBody Product product){
         return new ResponseEntity<>(productService.save(product), HttpStatus.CREATED);
     }
 
     @DeleteMapping("/delete/{productId}")
-    @ApiOperation("Delete a product by ID || Elimina un producto por su ID")
+    @ApiOperation(value = "Delete a product by ID || Elimina un producto por su ID", authorizations = { @Authorization(value="JWT") })
     public ResponseEntity delete(@PathVariable("productId") int productId){
         if (productService.delete(productId)){
             return new ResponseEntity(HttpStatus.OK);
